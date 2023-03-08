e8cc5ee2fc68488b0416cc29f9ac85f30057b15e
Walter <german@alumni.ucsf.edu>
2023-03-07 18:38:28 -0800

    Config file changes from Walter

--- meappy/meappy/configuration.py
+++ meappy/meappy/configuration.py
@@ -4,22 +4,25 @@ from os import path
 from pathlib import Path
 
 
-meappy_path = Path( __file__ ).parent.absolute()
-meap_path = meappy_path.parent.parent.absolute()
-base_path = meap_path.parent.absolute()
-params_path = path.join(meap_path, 'parameter_examples')
-
 _HOME = Path.home()
 # examples of home paths for Windows and Mac OS:
 # C:\Users\\fieldslab2\
 # /Users/walter
 
+meappy_path = Path( __file__ ).parent.absolute()
+meap_path = meappy_path.parent.parent.absolute()
+base_path = meap_path.parent.absolute()
 data_base_path = path.join(_HOME, 'Data')
         # /med64/experiment/VTA_NMDA/20211005_17h33m55s
+    
+params_examples = path.join(meap_path, 'parameter_examples')  # for out-of-box examples using github repo
+params_path = path.join(data_base_path, 'meap/experiment_log')
+
+# box shared directory
 box_base_path = path.join(_HOME, '/Library/CloudStorage/Box-Box')
 box_output_path = path.join(box_base_path, 'meap_output')
 
-expt_xl_file = 'MED64_ExperimentsForAnalysis.xls'
+expt_xl_file = 'MED64_ExperimentsForAnalysis.xlsx'
 
 # These configuration files can be set up for each computer
 # use python dataclasses
@@ -27,8 +30,8 @@ USER_PATHS = {
     "meappy_data": {
         "base": base_path,
         "meap": meap_path,
-        "output": path.join(base_path, 'meap_output'),
-        "exp_xlsx": path.join(params_path, expt_xl_file), 
+        "output": path.join(data_base_path, 'meap'),  # path.join(base_path, 'meap_output'),
+        "exp_xlsx": path.join(params_path, expt_xl_file), # path.join(params_path, expt_xl_file), 
         "params_template": params_path,
         "data_dir": path.join(data_base_path, 'med64/experiment/VTA_NMDA/20211005_17h33m55s'),
         "phy_export": path.join(data_base_path, 'meap/experiment'),
@@ -40,35 +43,35 @@ USER_PATHS = {
         "data_dir": r"C:\Users\\fieldslab2\\Desktop\\Lab\\MatLab\\MED64_Data\\experiment\\",
         "product_dir": r"C:\Users\\fieldslab2\\Desktop\\Lab\\MatLab\\MED64_Data\\product\\",
     },
-    "walter": {
-        "base": base_path,
-        "meap": meap_path,
-        "output": path.join(base_path, 'meap_output'),
-        "exp_xlsx": path.join(params_path, expt_xl_file),
-        # "data_dir": r"/Users/walter/Data/med64/experiment/",
-        # "product_dir": r"/Users/walter/Data/med64/product/",
-    },
-    "walter_box": {
-        "box": box_base_path,
-        "meap": meap_path,
-        "output":box_output_path,
-        "exp_xlsx": path.join(params_path, expt_xl_file),
-    },
-    "walter_data-v0": {
-        "meap": r"/Users/walter/Src/meap",
-        "output": r"/Users/walter/Src/meap_yaml_output",
-        "exp_xlsx": r"/Users/walter/Src/meap/parameter_examples/MED64_ExperimentsForAnalysis.xls",
-        # "data_dir": r"/Users/walter/Data/margolis/MED64_exampledata_09-22/",
-        # "product_dir": r"/Users/walter/Data/med64/product/",
-    },
 }
 
 USER = "meappy_data"  #"walter"  #  'elayne'  # walter_box
+COMPOSITE_ROW_ID = ('Date', 'Slice #')  # These are column names of row info used to create composite row_id
+
 
 XL_TAB = "VTA_EM1_Dose_Response"  #'LHb' # 'VTA_NMDA_Apamin' #'VTA_NMDA'
 # 'HbL' # 'HB_139_DAMGO'
 
 # [('date', 'Date'), ('cut_by', 'Cut by'), ('run_by', 'Run by'), ('region', 'Recording  Region'), ('project', 'Slice Location'), ('experiment_type', 'Project'), ('drugs_dose_used', 'Experiment Type'), ('vendor_batch', 'Drugs Used'), ('slice_time', 'Vendor/Batch'), ('is_photo_saved', 'Slice #'), ('notes_issues', 'Photo saved'), ('is_exported', 'Notes/Problems/Issues'), ('is_sorted', 'Exported?'), ('notes', 'Analyzed?')]
+
+
+XLCOL = {'date': 'date',
+    'cut_by': 'cut_by',
+    'run_by': 'run_by',
+    'region': 'recording__region',
+    'slice_location': 'slice_location',
+    'project': 'project',
+    'experiment_type': 'experiment_type',
+    'drugs_dose_used': 'drugs_used',
+    'vendor_batch': 'vendor_batch',
+    'slice_time': 'slice',
+    'is_photo_saved': 'photo_saved',
+    'notes_issues': 'notes_problems_issues',
+    'is_exported': 'exported',
+    'is_sorted': 'analyzed',
+    'notes': 'notes'
+}
+
 
 XL_COLS = [
     "date",
@@ -88,22 +91,22 @@ XL_COLS = [
     "notes",
 ]
 
-XL_COLS_old = [
-    "date",
-    "cut_by",
-    "run_by",
-    "region",
-    "project",
-    "experiment_type",
-    "drugs_dose_used",
-    "vendor_batch",
-    "slice_time",
-    "is_photo_saved",
-    "notes_issues",
-    "is_exported",
-    "is_sorted",
-    "notes",
-]
+# XL_COLS_old = [
+#     "date",
+#     "cut_by",
+#     "run_by",
+#     "region",
+#     "project",
+#     "experiment_type",
+#     "drugs_dose_used",
+#     "vendor_batch",
+#     "slice_time",
+#     "is_photo_saved",
+#     "notes_issues",
+#     "is_exported",
+#     "is_sorted",
+#     "notes",
+# ]
 
 # ROW_ID_LIST = ['20211006_15h58m38s', '20211005_17h33m55s'] #, '20211006_15h58m38s']  # NMDA_Apamin
 # ROW_ID_LIST = ['20200825_12h24m37s', '20200825_13h36m25s'] # LHb
