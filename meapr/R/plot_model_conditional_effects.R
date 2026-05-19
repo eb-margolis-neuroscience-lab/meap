380253c2485b72185dc39783d2bb5e6a422ddaae
Matt O'Meara <maom@ltfpa140.local>
2026-05-18 20:15:57 -0400

    Fix similar issues in one more plot

--- meapr/R/plot_model_conditional_effects.R
+++ meapr/R/plot_model_conditional_effects.R
@@ -1,13 +1,17 @@
 #' Plot Model Conditional Effects
 #'
 #' @param model_fit [brms::brmsfit] model object
-#' @param plot_width `numeric` width of the output plot
-#' @param plot_height `numeric` height of the output plot
-#' @param verbose `logical` print out verbose output
+#' @param model_tag `character` tag for the model
+#' @param extra_layers `list` extra ggplot2 layers to be added to the plot
+#'   before saving it.
+#' @param plot_width `numeric` width of the output plot.
+#' @param plot_height `numeric` height of the output plot.
+#' @param output_base `character` where to output plots.
+#' @param verbose `logical` print out verbose output.
 #'
 #' @returns: [ggplot2::ggplot] of the plot and it saves the result to
-#'   `product/plots/firing_qqplot_by_treatment_<experiment_tag>_<date_code>.(pdf|png)`
-#'   It save both .pdf and .png because it's easier to email etc small pngs
+#'   `<output_base>/marginal_effects_<model_tag>_<date_code>.(pdf|png)`
+#'   It save both .pdf and .png because it's easier to email etc small png files
 #'   while for use in an a manuscript having the vector version means that it`
 #'   can be tweaked with illustrator
 #'
@@ -15,12 +19,24 @@
 plot_model_conditional_effects <- function(
   model_fit,
   model_tag,
+  extra_layers = list(),
   plot_width = 6,
   plot_height = 6,
   output_base = "product/plots",
   verbose = FALSE,
   ...) {
 
+  if (!dir.exists(output_base)) {
+    if (verbose) {
+      cat("creating output directory '", output_base, "'\n", sep = "")
+    }
+    dir.create(
+      output_base,
+      showWarnings = FALSE,
+      recursive = TRUE)
+  }
+  
+  
   marginal_effects <- brms::conditional_effects(model_fit, ...)
 
   p <- plot(
@@ -30,19 +46,10 @@ plot_model_conditional_effects <- function(
     ggplot2::theme_bw() +
     ggplot2::ggtitle(
       "Model Fit Marginal Effects",
-      subtitle = model_tag)
+      subtitle = model_tag) +
+    extra_layers
 
   if (!is.null(output_base)) {
-    if (!dir.exists(output_base)) {
-      if (verbose) {
-        cat("creating output directory '", output_base, "'\n", sep = "")
-      }
-      dir.create(
-        output_base,
-        showWarnings = FALSE,
-        recursive = TRUE)
-    }
-
     pdf_path <- paste0(
       output_base, "/marginal_effects_", model_tag, "_", date_code(), ".pdf")
     if (verbose) {
